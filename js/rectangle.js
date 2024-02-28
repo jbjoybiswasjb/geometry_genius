f730572e5193cb3c80c38ad5d96bb1d442ea7ba3
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-02-28 13:57:11 +0600

    Rectangle Area Calculated.

--- js/rectangle.js
+++ js/rectangle.js
@@ -3,4 +3,19 @@ function calculateRectangleArea() {
     const inputFieldRectangleWidth = document.getElementById('rectangle_w');
     const rectangleWidthString = inputFieldRectangleWidth.value;
     const rectangleWidthNumber = parseFloat(rectangleWidthString);
+
+    // Rectangle Height.
+    const rectangleHeightField = document.getElementById('rectangle_l');
+    const rectangleHeightValue = rectangleHeightField.value;
+    const rectangleHeightNumber = parseFloat(rectangleHeightValue);
+
+    // Rectangle Area.
+    const rectangleArea = rectangleWidthNumber * rectangleHeightNumber;
+
+    // Rectangle area show in html document.
+    const rectangleAreaField = document.getElementById('rectangle_area');
+    rectangleAreaField.innerText = rectangleArea;
+
+    inputFieldRectangleWidth.value = '';
+    rectangleHeightField.value = '';
 }
